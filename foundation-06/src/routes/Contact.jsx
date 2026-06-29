17095b91884c6f1445106427693b45ac88ceb529
Aman <amankrdev28@gmail.com>
2026-06-29 19:52:08 +0530

    feat: initialize TanStack Router

---
+++ foundation-06/src/routes/Contact.jsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/Contact')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div>Hello "/Contact"!</div>
+}
+  
